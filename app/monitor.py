6c44921634cbf0e146cd4fbadeb3f6a4413b55cb
SŞ <ss@SSs-MacBook-Pro.local>
2026-01-07 17:07:25 +0300

    Disable network filtering (debug) and bump version to v8

--- app/monitor.py
+++ app/monitor.py
@@ -88,8 +88,8 @@ class SystemMonitor:
         rx = 0
         tx = 0
         for iface, stats in io.items():
-            if iface.startswith(('lo', 'docker', 'veth', 'br-', 'dummy')):
-                continue
+            # if iface.startswith(('lo', 'docker', 'veth', 'br-', 'dummy')):
+            #    continue
             rx += stats.bytes_recv
             tx += stats.bytes_sent
         # Return object compatible with psutil structure
